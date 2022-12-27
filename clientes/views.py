8ce7c1f1ed46b903eb09d2031a6c3f86c0088d67
Felipe Abreu <felipeabreu.rj@gmail.com>
2022-12-27 10:57:40 -0300

    validacao do tamanho do cpf

--- clientes/views.py
+++ clientes/views.py
@@ -2,8 +2,9 @@ from rest_framework import viewsets
 from clientes.serializers import ClienteSerializer
 from clientes.models import Cliente
 
+
 class ClientesViewSet(viewsets.ModelViewSet):
     """Listando clientes"""
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
-
+    http_method_names = ['get', 'post', 'put', 'patch', 'delete']
