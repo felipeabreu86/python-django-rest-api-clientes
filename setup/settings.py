1611ac5f9e884068cf5099c0a99ef8d1b0759665
Felipe Abreu <felipeabreu.rj@gmail.com>
2022-12-27 15:21:03 -0300

    definindo uma paginação para a api

--- setup/settings.py
+++ setup/settings.py
@@ -122,3 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Rest Framework Settings
+# https://www.django-rest-framework.org/
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
