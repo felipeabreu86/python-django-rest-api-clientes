9e3e096f3d2387a925847741133132ea0e90a8c3
Felipe Abreu <felipeabreu.rj@gmail.com>
2022-12-28 15:35:52 -0300

    atualizando o parametro list_per_page da classe Clientes

--- clientes/admin.py
+++ clientes/admin.py
@@ -9,7 +9,7 @@ class Clientes(admin.ModelAdmin):
     list_filter = ('ativo',)
     list_editable = ('ativo',)
     ordering = ('nome',)
-    list_per_page = 25
+    list_per_page = 10
 
 
 admin.site.register(Cliente, Clientes)
